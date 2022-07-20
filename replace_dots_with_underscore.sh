f676aec93673ab2156e7f750828242a6e4b3d016
neiladhikari <44908961+neiladhikari@users.noreply.github.com>
2022-07-20 16:06:29 -0700

    Update replace_dots_with_underscore.sh

--- replace_dots_with_underscore.sh
+++ replace_dots_with_underscore.sh
@@ -2,7 +2,7 @@
 #remove all the dots in the filename except the one before the extension and replace them with underscore.
 #preserves the dot before the extension and also preserves the extension.
 #works with any extension and any mix of extensions in the filenames.
-#eg. change "Image 2022-07-08 at 11.09.24 PM.png" to "Screen Shot 2022-07-08 at 11_09_24 PM.png"
+#eg. change "Image 2022-07-08 at 11.09.24 PM.png" to "Image 2022-07-08 at 11_09_24 PM.png"
 
 for f in *.*;
 do pre="${f%.*}";
